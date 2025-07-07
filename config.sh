d2da2336877a7450bb5cd80513c08effa440b38c
Ishwar Mudraje <s8ismudr@stud.uni-saarland.de>
2025-07-07 15:19:07 +0200

    Adding tools to path

--- config.sh
+++ config.sh
@@ -1,2 +1,3 @@
 python -m venv .venv && source .venv/bin/activate
 pip install -r requirements.txt
+export PATH=$PATH:/home/$USER/.espressif/tools/xtensa-lx106-elf/esp-2020r3-49-gd5524c1-8.4.0/xtensa-lx106-elf/bin/
